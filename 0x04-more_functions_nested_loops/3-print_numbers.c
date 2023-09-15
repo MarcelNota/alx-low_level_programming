26cf2624877af1cfddabe25f4e3b9b46a136f59e
MarcelNota <n3gr1t0.mn@gmail.com>
2023-09-14 17:52:00 -0700

    adition

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Print_numbers - prints
+ * Return: notthing
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a < 0; a++)
+{
+	_putchar(a + '0');
+}
+_putchar('\n');
+}
