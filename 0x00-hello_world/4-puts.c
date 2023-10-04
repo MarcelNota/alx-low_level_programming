f7fe7b38678730cef969a4683f9f0011fdfeb6e8
MarcelNota <128192970+MarcelNota@users.noreply.github.com>
2023-10-04 18:17:48 +0200

    adding 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main- initialization
+ *
+ * Return:  0 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
