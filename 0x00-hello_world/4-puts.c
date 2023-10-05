038fc8db8b148a3c7300567350ca927ec2e962e5
MarcelNota <n3gr1t0.mn@gmail.com>
2023-10-05 15:18:34 -0700

    correction of 4-puts and 6-size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  * main- initialization
  *
- * Return:  0 
- */
-
+ * Return:0
+ **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
