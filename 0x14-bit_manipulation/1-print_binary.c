7b7dabfcbb0ca575b6739d349bc970a971789379
MarcelNota <n3gr1t0.mn@gmail.com>
2023-11-25 02:15:55 +0200

    correction of file 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
-*print_binary - displays
-*@n: nr
+*print_binary - displayed
+*@n: nnr
 *Return: 0
 */
 
 void print_binary(unsigned long int n)
 {
 if (n > 1)
-print_binary(n >> 1);
-putchar((n & 1) + '0');
+	print_binary(n >> 1);
+
+_putchar((n & 1) + '0');
 }
