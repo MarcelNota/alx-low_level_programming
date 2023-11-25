69c6977f010ec6fe54dea172c1a5cb1b021bfc17
MarcelNota <n3gr1t0.mn@gmail.com>
2023-11-25 02:10:47 +0200

    finally

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,13 +8,10 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int decimal = 0;
 int str_len = 0, base = 1;
-
 if (!check_valid_string(b))
 return (0);
-
 while (b[str_len] != '\0')
 str_len++;
-
 while (str_len)
 {
 decimal += ((b[str_len - 1] - '0') * base);
@@ -23,7 +20,6 @@ str_len--;
 }
 return (decimal);
 }
-
 /**
  * check_valid_string - the binary
  * @b: hs
@@ -33,7 +29,6 @@ int check_valid_string(const char *b)
 {
 if (b == NULL)
 return (0);
-
 while (*b)
 {
 if (*b != '1' && *b != '0')
