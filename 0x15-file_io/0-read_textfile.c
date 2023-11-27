e7307ac25ca581ed5a1c111030aa0c0946212adf
MarcelNota <n3gr1t0.mn@gmail.com>
2023-11-28 00:41:19 +0200

    file 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,17 +7,22 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+ssize_t o, r, w;
 char *buffer;
-ssize_t a;
-ssize_t l;
-ssize_t e;
-a = open(filename, O_RDONLY);
-if (a == -1)
+if (filename == NULL)
 return (0);
 buffer = malloc(sizeof(char) * letters);
-e = read(a, buffer, letters);
-l = write(STDOUT_FILENO, buffer, e);
+if (buffer ==  NULL)
+return (0);
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
+if (o == -1 || r == -1 || w == -1 || w != r)
+{
 free(buffer);
-close(a);
-return (l);
+return (0);
+}
+free(buffer);
+close(o);
+return (w);
 }
